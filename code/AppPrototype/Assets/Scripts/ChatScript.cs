292057b7430159606de226d4afa8808ce353178a
Fenther586 <cammy.wilson586@gmail.com>
2023-07-04 01:24:15 +0100

    Redid the naviagation and got app-wide recolour working. Also bot now speaks better

--- code/AppPrototype/Assets/Scripts/ChatScript.cs
+++ code/AppPrototype/Assets/Scripts/ChatScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 
 public class ChatScript : MonoBehaviour
@@ -11,6 +12,35 @@ public class ChatScript : MonoBehaviour
     public void addUserChat(string chat)
     {
         Instantiate(userPrefab, transform.GetChild(0).GetChild(0).transform).GetComponentInChildren<TMPro.TextMeshProUGUI>().text = chat;
+    }
+
+
+    public void addBotChat(string chat)
+    {
+        StartCoroutine(addText(chat));
+    }
+
+    float timeBetweenWords = 0.08f;
+    IEnumerator addText(string text)
+    {
+        string[] tokens = text.Split(' ');
+        GameObject textBox = Instantiate(botPrefab, transform.GetChild(0).GetChild(0).transform);
+        textBox.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = "";
+        foreach (string token in tokens)
+        {
+            textBox.GetComponentInChildren<TMPro.TextMeshProUGUI>().text += token + " ";
+            yield return new WaitForSeconds(timeBetweenWords + Random.Range(-timeBetweenWords, timeBetweenWords));
+        }
+    }
+
+    string botText = "Hello, I am a bot. I am here to help you with your problems. What is your name?";
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            addBotChat(botText);
+        }
     }
 
 }
--- code/AppPrototype/Assets/Scripts/TextInput.cs
+++ code/AppPrototype/Assets/Scripts/TextInput.cs
@@ -8,33 +8,12 @@ public class TextInput : MonoBehaviour
 {
     public GameObject chatView;
 
-
-    //public TMP_InputField mainInputField;
-
-    //// Checks if there is anything entered into the input field.
-    //void LockInput(TMP_InputField input)
-    //{
-    //    if (input.text.Length > 0)
-    //    {
-    //        Debug.Log("Text has been entered");
-    //    }
-    //    else if (input.text.Length == 0)
-    //    {
-    //        Debug.Log("Main Input Empty");
-    //    }
-    //}
-
-    //public void Start()
-    //{
-    //    //Adds a listener that invokes the "LockInput" method when the player finishes editing the main input field.
-    //    //Passes the main input field into the method when "LockInput" is invoked
-    //    mainInputField.onEndEdit.AddListener(delegate { LockInput(mainInputField); });
-    //}
-
-
-
     public void sendChat()
     {
+        if (GetComponent<TMPro.TMP_InputField>().text.Trim() == "")
+        {
+            return;
+        }
         chatView.GetComponent<ChatScript>().addUserChat(GetComponent<TMPro.TMP_InputField>().text);
         GetComponent<TMPro.TMP_InputField>().text = "";
     }
