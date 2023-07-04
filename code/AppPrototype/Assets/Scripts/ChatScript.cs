6d4b1437085c38f386b069a174a11c3302db94c4
Fenther586 <cammy.wilson586@gmail.com>
2023-07-04 17:56:41 +0100

    Mascot customisation

--- code/AppPrototype/Assets/Scripts/ChatScript.cs
+++ code/AppPrototype/Assets/Scripts/ChatScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChatScript : MonoBehaviour
 {
@@ -11,7 +12,10 @@ public class ChatScript : MonoBehaviour
 
     public void addUserChat(string chat)
     {
-        Instantiate(userPrefab, transform.GetChild(0).GetChild(0).transform).GetComponentInChildren<TMPro.TextMeshProUGUI>().text = chat;
+        GameObject newChat = Instantiate(userPrefab, transform.GetChild(0).GetChild(0).transform);
+        newChat.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = chat;
+
+        newChat.transform.GetChild(0).GetComponent<ContentSizeFitter>().SetLayoutVertical();
     }
 
 
